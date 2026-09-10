24e78b661df951e07c9aa3a294411abcb2965ac0
Rajagopal <varunraj6081@gmail.com>
2026-09-11 00:53:29 +0530

    Code Changes

--- dist/med-project/browser/chunk-E4ACPZTN.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_html2canvas
-} from "./chunk-MI5K3SR6.js";
-import "./chunk-YP43Q66R.js";
-export default require_html2canvas();
-//# sourceMappingURL=chunk-E4ACPZTN.js.map
--- dist/med-project/browser/chunk-OCZWZWFA.js
+++
@@ -1,9 +0,0 @@
-// src/app/shared/api.config.ts
-var API_BASE_URL = "http://localhost:3000/api";
-var CLIENT_CODE = "MEDOREX001";
-
-export {
-  API_BASE_URL,
-  CLIENT_CODE
-};
-//# sourceMappingURL=chunk-OCZWZWFA.js.map
